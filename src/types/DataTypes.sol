b8fec3a7169b4d08e9f52b2fc260eac20744b6ac
NouDaimon <peric.iason@gmail.com>
2023-10-02 23:17:11 +0300

    feat: DataTypes file

---
+++ src/types/DataTypes.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: UNLICENSED
+
+import { IERC20 } from "@openzeppelin/contracts/interfaces/IERC20.sol";
+
+pragma solidity ^0.8.18;
+
+/// @title DataTypes
+/// @notice Contains all structs used in the Integrated Liquidity Market contract suite
+
+/// @dev contains all data relating to the collateral ratio
+struct CollateralRatio {
+    /// @dev mininimum value of the collateral ratio
+    uint256 min;
+    /// @dev maximum value of the collateral ratio
+    uint256 max;
+    /// @dev target (ideal) value of the collateral ratio
+    uint256 target;
+}
+
+/// @dev contains all data pertaining to the current position state of the strategy
+struct Position {
+    /// @dev wrapped adddress of collateralized asset of leverage strategy
+    IERC20 collateralAsset;
+    /// @dev wrapped address of borrowed asset of leverage strategy
+    IERC20 borrowedAsset;
+    /// @dev collateral value in underlying (USD)
+    uint256 collateral;
+    /// @dev debt value in underlying (USD)
+    uint256 debt;
+}
