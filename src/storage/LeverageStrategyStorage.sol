0fbf14cca3a5c237f27be2cb007c6c93a401c1b9
NouDaimon <peric.iason@gmail.com>
2023-10-12 00:08:41 +0300

    feat: initial storage layout library

---
+++ src/storage/LeverageStrategyStorage.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.18;
+
+import { CollateralRatio } from "../types/DataTypes.sol";
+
+library LeverageStrategyStorage {
+    /// @dev struct containing all state for the LeverageStrategy contract
+    struct Layout {
+        /// @dev struct encapsulating min/max bounds and target values for the collateral ratio
+        /// TODO: decide on whether to be 1e8 or 1e18 or 1e27
+        CollateralRatio collateralRatio;
+        /// @dev error margin on specific target collateral ratio passed in function calls
+        uint256 ratioMargin;
+    }
+
+    bytes32 internal constant STORAGE_SLOT =
+        keccak256('seamless.contracts.storage.LeverageStrategy');
+
+    function layout() internal pure returns (Layout storage l) {
+        bytes32 slot = STORAGE_SLOT;
+        assembly {
+            l.slot := slot
+        }
+    }
+}
