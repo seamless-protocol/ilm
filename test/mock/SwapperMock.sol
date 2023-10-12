5db7474df6ab28f28924f0e12db981ffece22ed0
NouDaimon <peric.iason@gmail.com>
2023-10-12 23:19:54 +0300

    build: increase swapper offsets, add price ratio effects

--- test/mock/SwapperMock.sol
+++ test/mock/SwapperMock.sol
@@ -5,19 +5,22 @@ pragma solidity ^0.8.18;
 import { IERC20 } from "@openzeppelin/contracts/interfaces/IERC20.sol";
 
 import { ISwapper } from "../../src/interfaces/ISwapper.sol";
+import { IOracleMock } from "./IOracleMock.sol";
 
 /// @title SwapperMock
 /// @dev Mocks the behavior of the Swapper contract
 contract SwapperMock is ISwapper {
     address public immutable collateralAsset;
     address public immutable borrowAsset;
-    uint256 public constant borrowToCollateralOffset = 1e7; // 10% assuming basis is 1e8
+    uint256 public constant borrowToCollateralOffset = 2.5e7; // 25% assuming basis is 1e8
     uint256 public constant collateralToBorrowOffset = 2e7; // 20% assuming basis is 1e8
     uint256 public constant BASIS = 1e8;
+    IOracleMock public oracle;
 
-    constructor(address _collateralAsset, address _borrowAsset)  {
+    constructor(address _collateralAsset, address _borrowAsset, address _oracleMock)  {
         collateralAsset = _collateralAsset;
         borrowAsset = _borrowAsset;
+        oracle = IOracleMock(_oracleMock);
     }
 
     /// @inheritdoc ISwapper
@@ -36,11 +39,16 @@ contract SwapperMock is ISwapper {
         uint256 _fromAmount,
         address payable _beneficiary
     ) external returns (uint256 _toAmount) {
-        _toAmount = _fromAmount;
         IERC20(_from).transferFrom(_beneficiary, address(this),  _fromAmount);
         
+        uint256 fromPriceUSD = oracle.getAssetPrice(_from);
+        uint256 toPriceUSD = oracle.getAssetPrice(_to);
+
+        _toAmount = _fromAmount * fromPriceUSD / toPriceUSD;
+
         /// mock account for the offset of DEX swaps 
-        _toAmount -= _fromAmount * offsetFactor(_from, _to) / BASIS;
+        _toAmount -= _toAmount * offsetFactor(_from, _to) / BASIS;
+
         IERC20(_to).transfer(_beneficiary, _toAmount);
     }
 
