eb37cf7272a4f47f2d10b71db1601ae0b4bf451e
duxxud <12116005+duxxud@users.noreply.github.com>
2023-10-13 16:59:33 +0200

    WrappedCbETH contract with permissioned deposits (#3)
    * feat: add WrappedCbETH contract with permissioned deposits
    
    * fix: change underlaying to be a public variable
    
    * fix: set depositor mapping visibility to public
    
    * feat: upgrade to OZ v5
    
    * upgrade openzeppelin
    
    * fix: natspec
    
    * feat: unit tests for WrappedCbETH contract
    
    * fix: using deal instead of mocked mint
    
    * fix: add reverting test; adds natspec to tests
    
    * fix: mock folder name

--- src/tokens/WrappedCbETH.sol
+++ src/tokens/WrappedCbETH.sol
@@ -1,6 +1,64 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.18;
 
-contract WrappedCbETH {
+import { IERC20, ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import { Ownable, Ownable2Step } from "@openzeppelin/contracts/access/Ownable2Step.sol";
+import { IWrappedERC20PermissionedDeposit} from "../interfaces/IWrappedERC20PermissionedDeposit.sol";
 
+/// @title WrappedCbETH
+/// @notice contract used to wrap underlying ERC20 token and mints the same amount of a wrapped token. 
+/// @notice this contract will be used to mainly to wrap cbETH and use it as a collateral in the lending pool
+/// @notice but only strategies contracts will be able to get wrapped token it and borrow against it.
+contract WrappedCbETH is IWrappedERC20PermissionedDeposit, ERC20, Ownable2Step {
+
+    /// @notice address of the underlying token which is wrapped
+    IERC20 public immutable underlying;
+
+    /// @notice map shows if address has pemission to wrap tokens
+    mapping(address => bool) public depositor;
+
+    modifier onlyDepositors {
+      if (!depositor[msg.sender]) {
+        revert NotDepositor(msg.sender);
+      }
+      _;
+    }
+
+    constructor(
+      string memory _name, 
+      string memory _symbol, 
+      IERC20 _underlyingToken,
+      address _initialOwner
+    ) ERC20(_name, _symbol) Ownable(_initialOwner) {
+      underlying = _underlyingToken;
+    }
+
+    /// @inheritdoc IWrappedERC20PermissionedDeposit
+    function deposit(uint256 amount) external override onlyDepositors {
+      SafeERC20.safeTransferFrom(underlying, msg.sender, address(this), amount);
+      _mint(msg.sender, amount);
+      emit Deposit(msg.sender, amount);
+    }
+
+    /// @inheritdoc IWrappedERC20PermissionedDeposit
+    function withdraw(uint256 amount) external override {
+      _burn(msg.sender, amount);
+      SafeERC20.safeTransfer(underlying, msg.sender, amount);
+      emit Withdraw(msg.sender, amount);
+    }
+
+    /// @inheritdoc IWrappedERC20PermissionedDeposit
+    function setDepositPermission(address account, bool toSet) external override onlyOwner {
+      depositor[account] = toSet;
+      emit SetDepositPermission(account, toSet);
+    }
+
+    /// @inheritdoc IWrappedERC20PermissionedDeposit
+    function recover() external override onlyOwner {
+      uint256 amountSurplus = underlying.balanceOf(address(this)) - totalSupply();
+      SafeERC20.safeTransfer(underlying, msg.sender, amountSurplus);
+      emit RecoverUnderlyingSurplus(msg.sender, amountSurplus);
+    }
 }
+
---
+++ test/unit/WrappedCbETH.t.sol
@@ -0,0 +1,130 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.18;
+
+import { Test } from "forge-std/Test.sol";
+import { IERC20 } from "@openzeppelin/contracts/interfaces/IERC20.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { WrappedCbETH, IWrappedERC20PermissionedDeposit } from "../../src/tokens/WrappedCbETH.sol";
+import { MockERC20 } from "../mocks/MockERC20.sol";
+
+/// @title WrappedCbETHTest
+/// @notice Unit tests for the WrappedCbETH contract
+contract WrappedCbETHTest is Test {
+    WrappedCbETH public wrappedCbETH;
+    MockERC20 public mockERC20;
+
+    address public alice = makeAddr("alice");
+
+    function setUp() public {
+        mockERC20 = new MockERC20("Mock", "M");
+        wrappedCbETH = new WrappedCbETH("WrappedMock", "WM", IERC20(mockERC20), address(this));
+
+        deal(address(mockERC20), address(alice), 100 ether);
+    }
+
+    /// @dev testing if owner and underlying token are set up correctly on the construction
+    function test_setUp() public {
+      assertEq(wrappedCbETH.owner(), address(this));
+      assertEq(address(wrappedCbETH.underlying()), address(mockERC20));
+    }
+
+    /// @dev test reverting if unpermissioned user try to deposit(wrap) the token
+    function test_deposit_revertNotDepositor() public {
+      vm.startPrank(alice);
+      uint256 amount = 10 ether;
+      vm.expectRevert(
+        abi.encodeWithSelector(IWrappedERC20PermissionedDeposit.NotDepositor.selector, alice)
+      );
+      wrappedCbETH.deposit(amount);
+      vm.stopPrank();
+    }
+
+    /// @dev test confirming permissions for deposits are set and unset correctly
+    function test_setDepositPermission() public {
+      assertEq(wrappedCbETH.depositor(alice), false);
+
+      wrappedCbETH.setDepositPermission(alice, true);
+      assertEq(wrappedCbETH.depositor(alice), true);
+
+      wrappedCbETH.setDepositPermission(alice, false);
+      assertEq(wrappedCbETH.depositor(alice), false);
+    }
+
+    /// @dev test confirming that permissioned user can deposit and receive correct amount of wrapped token
+    function test_deposit() public {
+      wrappedCbETH.setDepositPermission(alice, true);
+
+      uint256 amountUnderlyingBefore = mockERC20.balanceOf(alice);
+      uint256 amountWrappedBefore = wrappedCbETH.balanceOf(alice);
+
+      uint256 depositAmount = 10 ether;
+      _deposit(alice, depositAmount);
+
+      assertEq(mockERC20.balanceOf(alice), amountUnderlyingBefore - depositAmount);
+      assertEq(wrappedCbETH.balanceOf(alice), amountWrappedBefore + depositAmount);
+    }
+
+    /// @dev test confirming that anybody can withdraw (unwrap) and receive correct amount of underlying token
+    function test_withdraw() public {
+      wrappedCbETH.setDepositPermission(alice, true);
+      uint256 depositAmount = 10 ether;
+      _deposit(alice, depositAmount);
+
+      uint256 amountUnderlyingBefore = mockERC20.balanceOf(alice);
+      uint256 amountWrappedBefore = wrappedCbETH.balanceOf(alice);
+
+      uint256 withdrawAmount = depositAmount;
+      _withdraw(alice, withdrawAmount);
+
+      assertEq(mockERC20.balanceOf(alice), amountUnderlyingBefore + withdrawAmount);
+      assertEq(wrappedCbETH.balanceOf(alice), amountWrappedBefore - withdrawAmount);
+
+      // test if can withdraw without depositPermission
+      _deposit(alice, depositAmount);
+      wrappedCbETH.setDepositPermission(alice, false);
+      _withdraw(alice, withdrawAmount);
+      assertEq(mockERC20.balanceOf(alice), amountUnderlyingBefore + withdrawAmount);
+      assertEq(wrappedCbETH.balanceOf(alice), amountWrappedBefore - withdrawAmount);
+    }
+
+    /// @dev test confirming that recovering of wrongly sent underlying tokens are possible by owner and receievs correct amount of underlying
+    function test_recover() public {
+      uint256 amount = 10 ether;
+
+      vm.startPrank(alice);
+      mockERC20.transfer(address(wrappedCbETH), amount);
+      vm.stopPrank();
+
+      assertEq(wrappedCbETH.totalSupply(), 0);
+      assertEq(mockERC20.balanceOf(address(this)), 0); 
+      assertEq(mockERC20.balanceOf(address(wrappedCbETH)), amount);
+
+      wrappedCbETH.recover();
+      assertEq(mockERC20.balanceOf(address(this)), amount);
+    }
+
+    /// @dev test reverting when non owner try to recover wrongly sent underlying tokens to the contract
+    function test_recover_revertNotOwner() public {
+      uint256 amount = 10 ether;
+      vm.startPrank(alice);
+      mockERC20.transfer(address(wrappedCbETH), amount);
+      vm.expectRevert(abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, address(alice)));
+      wrappedCbETH.recover();
+      vm.stopPrank();
+    }
+
+    /// @dev depositing funds to the wrapper contract to get wrapped token
+    function _deposit(address account, uint256 depositAmount) internal {
+      vm.startPrank(account);
+      mockERC20.approve(address(wrappedCbETH), depositAmount);
+      wrappedCbETH.deposit(depositAmount);
+      vm.stopPrank();
+    }
+
+    /// @dev withdrawing funds from the wrapper contract to unwrap the token and get underlying
+    function _withdraw(address account, uint256 withdrawAmount) internal {
+      vm.startPrank(account);
+      wrappedCbETH.withdraw(withdrawAmount);
+      vm.stopPrank();
+    }
+}
