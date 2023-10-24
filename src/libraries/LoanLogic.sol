f3ca2d749965ad8010ab1f1cf6fa1bcf6a1aa00f
duxxud <12116005+duxxud@users.noreply.github.com>
2023-10-24 23:29:43 +0200

    `LoanLogic` library (#6)
    * chore: openzeppelin v5 ugrade (removing old)
    
    * forge install: openzeppelin-contracts
    
    v5.0.0
    
    * forge install: aave-v3-core
    
    v1.19.1
    
    * chore: add aave lib remmaping
    
    * feat: LoanLogic library
    
    * feat: tests for LoanLogic library
    
    * feat: add maxBorrow and maxWithdraw tests
    
    * feat: add LoanLogic revert tests on borrow/withdraw above max available
    
    * chore: natspec
    
    * Update src/libraries/LoanLogic.sol
    
    Co-authored-by: NouDaimon <81045441+NouDaimon@users.noreply.github.com>
    
    * Update test/unit/LoanLogic.t.sol
    
    Co-authored-by: NouDaimon <81045441+NouDaimon@users.noreply.github.com>
    
    * Update test/unit/LoanLogic.t.sol
    
    Co-authored-by: NouDaimon <81045441+NouDaimon@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: NouDaimon <81045441+NouDaimon@users.noreply.github.com>
    
    * Squashed commit of the following:
    
    commit b8e5adfc7eb6d51c1f87cc740def6af23c3b57b7
    Author: DuXXuD <duxxud@gmail.com>
    Date:   Mon Oct 16 22:18:28 2023 +0200
    
        feat: adds ILendingPoolAdapter, used instead of LoanLogic as library
    
    * Revert "Squashed commit of the following:"
    
    This reverts commit a65d8e46d14f1f8ff74506c949cfaa1a7d7eef36.
    
    * feat: add LendingPool type for pool config
    
    * feat: updated tests for LoanLogic lib
    
    * Apply suggestions from code review
    
    Co-authored-by: NouDaimon <81045441+NouDaimon@users.noreply.github.com>
    
    * fix: add natspec to TestConstants
    
    * feat: add fuzz borrow and withdraw tests
    
    * fix: fixing LoanLogic natspec
    
    * fix: add env var to gh action
    
    * feat: add .env.sample
    
    ---------
    
    Co-authored-by: NouDaimon <81045441+NouDaimon@users.noreply.github.com>

--- src/libraries/LoanLogic.sol
+++ src/libraries/LoanLogic.sol
@@ -3,45 +3,87 @@
 pragma solidity ^0.8.18;
 
 import { IERC20 } from "@openzeppelin/contracts/interfaces/IERC20.sol";
+import { IPoolAddressesProvider } from "@aave/contracts/interfaces/IPoolAddressesProvider.sol";
+import { IPool } from "@aave/contracts/interfaces/IPool.sol";
+import { DataTypes } from "@aave/contracts/protocol/libraries/types/DataTypes.sol";
+import { PercentageMath } from "@aave/contracts/protocol/libraries/math/PercentageMath.sol";
+import { LoanState, LendingPool } from "../types/DataTypes.sol";
 
 /// @title LoanLogic
-/// @notice Contains all logic required for managing the loan position
+/// @notice Contains all logic required for managing the loan position on the Seamless protocol
+/// @dev when calling pool functions, `onBehalfOf` is set to `address(this)` which, in most cases,
+/// @dev represents the strategy vault contract.
 library LoanLogic {
-    /// @dev collateralizes an amount of underlying asset in AaveV3 via depositing assets into Aave lending pool
+    /// @dev used for availableBorrowsBase and maxWithdrawAmount to decrease them by 0.01%
+    /// @dev because precision issues on converting to asset amounts can revert borrow/withdraw on lending pool
+    uint256 public constant MAX_AMOUNT_PERCENT = 99_99;
+
+    /// @notice collateralizes an amount of the given asset via depositing assets into Seamless lending pool
+    /// @param lendingPool struct which contains lending pool setup (pool address and interest rate mode)
     /// @param asset address of collateral asset
     /// @param amount amount of asset to collateralize
-    function supply(IERC20 asset, uint256 amount) external { }
+    /// @return state loan state after supply call
+    function supply(LendingPool memory lendingPool, IERC20 asset, uint256 amount) external returns(LoanState memory state) {
+        lendingPool.pool.supply(address(asset), amount, address(this), 0);
+        return getLoanState(lendingPool);
+    }
 
-    /// @dev withdrawing collateral from the lending pool
+    /// @notice withdraws collateral from the lending pool
+    /// @param lendingPool struct which contains lending pool setup (pool address and interest rate mode)
     /// @param asset address of collateral asset
     /// @param amount amount of asset to withdraw
-    function withdraw(IERC20 asset, uint256 amount) external { }
+    /// @return state loan state after supply call
+    function withdraw(LendingPool memory lendingPool, IERC20 asset, uint256 amount) external returns(LoanState memory state) {
+        lendingPool.pool.withdraw(address(asset), amount, address(this));
+        return getLoanState(lendingPool);
+    }
 
-    /// @dev borrows an amount of borrowed asset from AaveV3
+    /// @notice borrows an amount of borrowed asset from the lending pool
+    /// @param lendingPool struct which contains lending pool setup (pool address and interest rate mode)
     /// @param asset address of borrowing asset
     /// @param amount amount of asset to borrow
-    function borrow(IERC20 asset, uint256 amount) external { }
+    /// @return state loan state after supply call
+    function borrow(LendingPool memory lendingPool, IERC20 asset, uint256 amount) external returns(LoanState memory state) {
+        lendingPool.pool.borrow(address(asset), amount, lendingPool.interestRateMode, 0, address(this));
+        return getLoanState(lendingPool);
+    }
 
-    /// @dev repays an amount of borrowed asset to AaveV3
+    /// @notice repays an amount of borrowed asset to the lending pool
+    /// @param lendingPool struct which contains lending pool setup (pool address and interest rate mode)
     /// @param asset address of borrowing asset
     /// @param amount amount of borrowing asset to repay
-    function repay(IERC20 asset, uint256 amount) external { }
+    /// @return state loan state after supply call
+    function repay(LendingPool memory lendingPool, IERC20 asset, uint256 amount) external returns(LoanState memory state) {
+        lendingPool.pool.repay(address(asset), amount, lendingPool.interestRateMode, address(this));
+        return getLoanState(lendingPool);
+    }
 
-    /// @dev returns the maximum value of available borrowing in USD for an account
-    /// @param account address of an acount
-    /// @return uint256 available value to borrow in USD
-    function maxBorrowAvailable(address account)
-        external
-        view
-        returns (uint256)
-    { }
+    /// @notice returns the current state of loan position on the Seamless Protocol lending pool for the caller's account
+    /// @notice all returned values are in USD value
+    /// @param lendingPool struct which contains lending pool setup (pool address and interest rate mode)  
+    /// @return state loan state after supply call
+    function getLoanState(LendingPool memory lendingPool) internal view returns(LoanState memory state) {        
+        (
+            uint256 totalCollateralBase,
+            uint256 totalDebtBase,
+            uint256 availableBorrowsBase,
+            /* currentLiquidationThreshold */,
+            uint256 ltv,
+            /* healthFactor */
+        ) = lendingPool.pool.getUserAccountData(address(this));
 
-    /// @dev returns the maximum value of collateral available to withdraw in USD for an account
-    /// @param account address of an acount
-    /// @return uint256 available value to withdraw in USD
-    function maxWithdrawAvailable(address account)
-        external
-        view
-        returns (uint256)
-    { }
+        uint256 maxWithdrawAmount = 
+            totalCollateralBase - PercentageMath.percentDiv(totalDebtBase, ltv);
+
+
+        availableBorrowsBase = PercentageMath.percentMul(availableBorrowsBase, MAX_AMOUNT_PERCENT);
+        maxWithdrawAmount = PercentageMath.percentMul(maxWithdrawAmount, MAX_AMOUNT_PERCENT);
+
+        return LoanState({
+            collateral: totalCollateralBase,
+            debt: totalDebtBase,
+            maxBorrowAmount: availableBorrowsBase,
+            maxWithdrawAmount: maxWithdrawAmount
+        });
+    }   
 }
