d094b3588c03126d1bb571c38f32e0a3dc41410b
NouDaimon <peric.iason@gmail.com>
2023-11-01 20:21:15 +0200

    feat: update storage library

--- src/storage/LoopStrategyStorage.sol
+++ src/storage/LoopStrategyStorage.sol
@@ -2,17 +2,23 @@
 
 pragma solidity ^0.8.18;
 
-import { CollateralRatio, StrategyAssets, LendingPool } from "../types/DataTypes.sol";
-import { IPoolAddressesProvider } from "@aave/contracts/interfaces/IPoolAddressesProvider.sol";
-import { IPool } from "@aave/contracts/interfaces/IPool.sol"; 
-import { IPriceOracleGetter } from "@aave/contracts/interfaces/IPriceOracleGetter.sol";
+import {
+    CollateralRatio,
+    StrategyAssets,
+    LendingPool
+} from "../types/DataTypes.sol";
+import { IPoolAddressesProvider } from
+    "@aave/contracts/interfaces/IPoolAddressesProvider.sol";
+import { IPool } from "@aave/contracts/interfaces/IPool.sol";
+import { IPriceOracleGetter } from
+    "@aave/contracts/interfaces/IPriceOracleGetter.sol";
 import { ISwapper } from "../interfaces/ISwapper.sol";
 
 library LoopStrategyStorage {
     /// @dev struct containing all state for the LoopStrategy contract
     struct Layout {
         /// @dev struct encapsulating collateral and borrow asset addresses
-        StrategyAssets strategyAssets;
+        StrategyAssets assets;
         /// @dev struct encapsulating min/max bounds and target values for the collateral ratio
         /// TODO: decide on whether to be 1e8 or 1e18 or 1e27
         CollateralRatio collateralRatioTargets;
