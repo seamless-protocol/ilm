0ba31d918ea5b8b7cc43f6ce125d74cfc24f5438
NouDaimon <peric.iason@gmail.com>
2023-11-01 15:49:46 +0200

    feat: update storage layout

--- src/storage/LeverageStrategyStorage.sol
+++ src/storage/LeverageStrategyStorage.sol
@@ -5,15 +5,23 @@ pragma solidity ^0.8.18;
 import { CollateralRatio, StrategyAssets } from "../types/DataTypes.sol";
 
 library LeverageStrategyStorage {
-    /// @dev struct containing all state for the LeverageStrategy contract
+    /// @dev struct containing all state for the LoopStrategy contract
     struct Layout {
         /// @dev struct encapsulating collateral and borrow asset addresses
-        StrategyAssets strategyAssets;
+        StrategyAssets assets;
         /// @dev struct encapsulating min/max bounds and target values for the collateral ratio
         /// TODO: decide on whether to be 1e8 or 1e18 or 1e27
-        CollateralRatio collateralRatio;
+        CollateralRatio collateralRatioTargets;
         /// @dev error margin on specific target collateral ratio passed in function calls
         uint256 ratioMargin;
+        /// @dev pool address provider for the Seamles Protocol lending pools
+        IPoolAddressesProvider poolAddressProvider;
+        /// @dev struct encapsulating address of the lending pool and configuration (interest rate mode)
+        LendingPool lendingPool;
+        /// @dev price oracle address
+        IPriceOracleGetter oracle;
+        /// @dev swapper address
+        ISwapper swapper;
     }
 
     bytes32 internal constant STORAGE_SLOT =
