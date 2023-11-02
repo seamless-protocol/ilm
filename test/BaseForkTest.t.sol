0a87dc013a61de40cdd6bf3977d6e2cf1bf7a8b1
NouDaimon <peric.iason@gmail.com>
2023-11-02 10:04:36 +0200

    build: pin fork block number

--- test/BaseForkTest.t.sol
+++ test/BaseForkTest.t.sol
@@ -12,7 +12,9 @@ abstract contract BaseForkTest is Test, TestConstants {
     /// @dev Fetches and stores the BASE MAINNET RPC URL from a local .env file using the passed string as a key.
     string internal BASE_RPC_URL = vm.envString("BASE_MAINNET_RPC_URL");
 
+    uint256 internal constant FORK_BLOCK_NUMBER = 5950437;
+
     /// @dev Identifier for the simulated Base fork.
     /// @notice Fork is created, available for selection, and selected by default.
-    uint256 internal baseFork = vm.createSelectFork(BASE_RPC_URL);
+    uint256 internal baseFork = vm.createSelectFork(BASE_RPC_URL, FORK_BLOCK_NUMBER);
 }
