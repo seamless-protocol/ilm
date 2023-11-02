c3c28a5582f364abdab7ed5572403ff1cf39c315
NouDaimon <peric.iason@gmail.com>
2023-11-02 10:54:13 +0200

    fix: typo

--- test/lib/RebalanceLogicContext.t.sol
+++ test/lib/RebalanceLogicContext.t.sol
@@ -58,7 +58,7 @@ abstract contract RebalanceLogicContext is BaseForkTest {
     uint256 internal constant TARGET_CR = 1.5e8;
     uint256 internal constant MIN_FOR_REBALANCE_CR = 134_444_444;
     uint256 internal constant MAX_FOR_REBALANCE_CR = 166_666_666;
-    uint256 internal constant MAX_FOR_WITHDRAW_REBALANCE_CR = 1.55e8;
+    uint256 internal constant MIN_FOR_WITHDRAW_REBALANCE_CR = 1.55e8;
     uint256 internal constant MAX_FOR_DEPOSIT_REBALANCE_CR = 1.45e8;
 
     /// @dev sets up auxiliary contracts and context for RebalanceLogic tests
