058b2436efee18468812db9813425a613c58dc7b
NouDaimon <peric.iason@gmail.com>
2023-12-04 16:25:51 +0200

    feat: initial implementation of `Swapper` contract

---
+++ src/swap/Swapper.sol
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.18;
+
+import {
+    Ownable2StepUpgradeable,
+    OwnableUpgradeable
+} from "@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
+import { Math } from "@openzeppelin/contracts/utils/math/Math.sol";
+import { IERC20 } from "@openzeppelin/contracts/interfaces/IERC20.sol";
+import { IERC20Metadata } from
+    "@openzeppelin/contracts/interfaces/IERC20Metadata.sol";
+import { SafeERC20 } from
+    "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+import { ISwapper } from "../interfaces/ISwapper.sol";
+import { SwapperStorage as Storage } from "../storage/SwapperStorage.sol";
+import { Step } from "../types/DataTypes.sol";
+
+/// @title Swapper
+/// @notice Routing contract for swaps across different DEXs
+contract Swapper is Ownable2StepUpgradeable, ISwapper {
+    /// @dev initializer function for Swapper contract
+    function Swapper_init(address _initialOwner) external initializer {
+        __Ownable_init(_initialOwner);
+    }
+
+    /// @inheritdoc ISwapper
+    function getRoute(IERC20 from, IERC20 to)
+        external
+        view
+        returns (Step[] memory steps)
+    {
+        return Storage.layout().route[from][to];
+    }
+
+    /// @inheritdoc ISwapper
+    function setRoute(IERC20 from, IERC20 to, Step[] calldata steps)
+        external
+        onlyOwner
+    {
+        Storage.Layout storage $ = Storage.layout();
+
+        for (uint256 i; i < steps.length; ++i) {
+            $.route[from][to].push(steps[i]);
+        }
+
+        emit RouteSet(from, to, steps);
+    }
+
+    /// @inheritdoc ISwapper
+    function offsetFactor(IERC20 from, IERC20 to)
+        external
+        view
+        returns (uint256 offsetUSD)
+    {
+        return Storage.layout().offsetUSD[from][to];
+    }
+
+    /// @inheritdoc ISwapper
+    function setOffsetFactor(IERC20 from, IERC20 to, uint256 offsetUSD)
+        external
+        onlyOwner
+    {
+        Storage.layout().offsetUSD[from][to] = offsetUSD;
+
+        emit OffsetFactorSet(from, to, offsetUSD);
+    }
+
+    /// @inheritdoc ISwapper
+    function swap(
+        IERC20 from,
+        IERC20 to,
+        uint256 fromAmount,
+        address payable beneficiary
+    ) external returns (uint256 toAmount) {
+        Step[] memory steps = Storage.layout().route[from][to];
+
+        // execute the swap for each swap-step in the route,
+        // updating `fromAmount` to be the amount received from
+        // each step
+        for (uint256 i; i < steps.length; ++i) {
+            fromAmount = steps[i].adapter.executeSwap(
+                steps[i].from, steps[i].to, fromAmount, payable(address(this))
+            );
+        }
+
+        // set the received amount as the amount received from the final
+        // step of the route
+        toAmount = fromAmount;
+
+        to.transfer(beneficiary, toAmount);
+    }
+}
