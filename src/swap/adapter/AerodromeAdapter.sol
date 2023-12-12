a3ca3768e94ef72a0380f7343f082c8bb49b75de
NouDaimon <peric.iason@gmail.com>
2023-12-12 12:46:13 +0200

    feat: `AerodromeAdapter` auxiliary functions and tests

--- src/swap/adapter/AerodromeAdapter.sol
+++ src/swap/adapter/AerodromeAdapter.sol
@@ -31,12 +31,21 @@ contract AerodromeAdapter is Ownable2StepUpgradeable, ISwapAdapter {
     /// @param router address of router
     event RouterSet(address router);
 
+    /// @notice emitted when set routes for a given swap are removed
+    /// @param from address to swap from
+    /// @param to addrses to swap to
+    event RoutesRemoved(IERC20 from, IERC20 to);
+
     /// @notice emitted when the swap routes for a token pair are set
     /// @param from first token of the pool
     /// @param to second token of the pool
     /// @param routes array of routes for swap
     event RoutesSet(IERC20 from, IERC20 to, IRouter.Route[] routes);
 
+    /// @notice initializing function of adapter
+    /// @param owner address of adapter owner
+    /// @param router address of Aerodrome router
+    /// @param factory address of Aerodrome pair factory
     function AerodromeAdapter__Init(
         address owner,
         address router,
@@ -61,8 +70,6 @@ contract AerodromeAdapter is Ownable2StepUpgradeable, ISwapAdapter {
         from.transferFrom(msg.sender, address(this), fromAmount);
 
         from.approve($.router, fromAmount);
-
-        console.log($.swapRoutes[from][to].length);
 
         uint256[] memory toAmounts = IRouter($.router).swapExactTokensForTokens(
             fromAmount,
@@ -104,6 +111,10 @@ contract AerodromeAdapter is Ownable2StepUpgradeable, ISwapAdapter {
         emit RouterSet(router);
     }
 
+    /// @notice sets routes for a given swap
+    /// @param from address of token to swap from
+    /// @param to address of tokent to swap to
+    /// @param routes routes for the swap
     function setRoutes(IERC20 from, IERC20 to, IRouter.Route[] memory routes)
         external
         onlyOwner
@@ -111,7 +122,7 @@ contract AerodromeAdapter is Ownable2StepUpgradeable, ISwapAdapter {
         Storage.Layout storage $ = Storage.layout();
 
         if ($.swapRoutes[from][to].length != 0) {
-            delete $.swapRoutes[from][to];
+            _removeRoutes(from, to);
         }
 
         for (uint256 i; i < routes.length; ++i) {
@@ -119,5 +130,57 @@ contract AerodromeAdapter is Ownable2StepUpgradeable, ISwapAdapter {
         }
 
         emit RoutesSet(from, to, routes);
+    }
+
+    /// @notice deletes existing routes for a given swap
+    /// @param from address of token route ends with
+    /// @param to address of token route starts with
+    function removeRoutes(IERC20 from, IERC20 to) external onlyOwner {
+        _removeRoutes(from, to);
+    }
+
+    /// @notice fetches the 'stable' status of a pool
+    /// @param from address of `from` token
+    /// @param to address of `to` token
+    /// @return status 'stable' status of pool
+    function getIsPoolStable(IERC20 from, IERC20 to)
+        external
+        view
+        returns (bool status)
+    {
+        return Storage.layout().isPoolStable[from][to];
+    }
+
+    /// @notice fetches the Aerodrome PoolFactory address
+    /// @return factory address of Aerodrome PoolFactory contract
+    function getPoolFactory() external view returns (address factory) {
+        return Storage.layout().poolFactory;
+    }
+
+    /// @notice fetches the Aerodrome Router address
+    /// @return router address of Aerodrome Router contract
+    function getRouter() external view returns (address router) {
+        return Storage.layout().router;
+    }
+
+    /// @notice fetches the swap routes for a given token swap
+    /// @param from address of `from` token
+    /// @param to address of `to` token
+    /// @return routes IRouter.Route struct array corresponding to the token swap
+    function getSwapRoutes(IERC20 from, IERC20 to)
+        external
+        view
+        returns (IRouter.Route[] memory routes)
+    {
+        return Storage.layout().swapRoutes[from][to];
+    }
+
+    /// @notice deletes existing routes for a given swap
+    /// @param from address of token route ends with
+    /// @param to address of token route starts with
+    function _removeRoutes(IERC20 from, IERC20 to) internal {
+        delete Storage.layout().swapRoutes[from][to];
+
+        emit RoutesRemoved(from, to);
     }
 }
--- test/unit/AerodromeAdapter.t.sol
+++ test/unit/AerodromeAdapter.t.sol
@@ -2,6 +2,10 @@
 pragma solidity ^0.8.18;
 
 import { IERC20 } from "@openzeppelin/contracts/interfaces/IERC20.sol";
+import {
+    Ownable2StepUpgradeable,
+    OwnableUpgradeable
+} from "@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
 
 import { BaseForkTest } from "../BaseForkTest.t.sol";
 import { AerodromeAdapter } from "../../src/swap/adapter/AerodromeAdapter.sol";
@@ -10,12 +14,42 @@ import { IRouter } from "../../src/vendor/aerodrome/IRouter.sol";
 /// @title AerodromeAdapterTEst
 /// @notice Unit tests for the AerodromeAdapter contract
 contract AerodromeAdapterTest is BaseForkTest {
+    ///////////////////////////////////
+    //////// REPLICATED EVENTS ////////
+    ///////////////////////////////////
+
+    /// @notice emitted when a value whether a pool is stable or not is set
+    /// @param from first token of the pool
+    /// @param to second token of the pool
+    /// @param status boolean value indicating pool stability
+    event IsPoolStableSet(IERC20 from, IERC20 to, bool status);
+
+    /// @notice emitted when the poolFactory address is set
+    /// @param factory address of poolFactory
+    event PoolFactorySet(address factory);
+
+    /// @notice emitted when the router address is set
+    /// @param router address of router
+    event RouterSet(address router);
+
+    /// @notice emitted when set routes for a given swap are removed
+    /// @param from address to swap from
+    /// @param to addrses to swap to
+    event RoutesRemoved(IERC20 from, IERC20 to);
+
+    /// @notice emitted when the swap routes for a token pair are set
+    /// @param from first token of the pool
+    /// @param to second token of the pool
+    /// @param routes array of routes for swap
+    event RoutesSet(IERC20 from, IERC20 to, IRouter.Route[] routes);
+
     IERC20 public WETH = IERC20(BASE_MAINNET_WETH);
     IERC20 public CbETH = IERC20(BASE_MAINNET_CbETH);
 
     uint256 swapAmount = 1 ether;
     address alice = makeAddr("alice");
     address public OWNER = makeAddr("OWNER");
+    address public NON_OWNER = makeAddr("NON_OWNER");
 
     AerodromeAdapter adapter;
 
@@ -53,5 +87,197 @@ contract AerodromeAdapterTest is BaseForkTest {
         uint256 newBalance = CbETH.balanceOf(alice);
 
         assertEq(newBalance - oldBalance, receivedCbETH);
+    }
+
+    function test_setRoutes_setsRoutesForASwap_and_emitsRoutesSetEvent()
+        public
+    {
+        assertEq(adapter.getSwapRoutes(WETH, CbETH).length, 0);
+
+        IRouter.Route[] memory routes = new IRouter.Route[](1);
+
+        routes[0] = IRouter.Route({
+            from: address(WETH),
+            to: address(CbETH),
+            stable: false,
+            factory: AERODROME_FACTORY
+        });
+
+        vm.prank(OWNER);
+
+        vm.expectEmit();
+        emit RoutesSet(WETH, CbETH, routes);
+
+        adapter.setRoutes(WETH, CbETH, routes);
+    }
+
+    function test_setRoutes_deletsPreviousRoute_and_setsRoutesForASwap()
+        public
+    {
+        assertEq(adapter.getSwapRoutes(WETH, CbETH).length, 0);
+
+        IRouter.Route[] memory routes = new IRouter.Route[](1);
+
+        routes[0] = IRouter.Route({
+            from: address(WETH),
+            to: address(CbETH),
+            stable: false,
+            factory: AERODROME_FACTORY
+        });
+
+        vm.prank(OWNER);
+        adapter.setRoutes(WETH, CbETH, routes);
+
+        vm.prank(OWNER);
+
+        vm.expectEmit();
+        emit RoutesRemoved(WETH, CbETH);
+
+        adapter.setRoutes(WETH, CbETH, routes);
+    }
+
+    function test_setRoutes_revertsWhen_calledByNonOwner() public {
+        IRouter.Route[] memory routes = new IRouter.Route[](1);
+
+        routes[0] = IRouter.Route({
+            from: address(WETH),
+            to: address(CbETH),
+            stable: false,
+            factory: AERODROME_FACTORY
+        });
+
+        vm.prank(NON_OWNER);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                OwnableUpgradeable.OwnableUnauthorizedAccount.selector,
+                NON_OWNER
+            )
+        );
+        adapter.setRoutes(WETH, CbETH, routes);
+    }
+
+    function test_removeRoutes_removesPreviouslySetRoutes_and_emitsRoutesRemovesEvent(
+    ) public {
+        IRouter.Route[] memory routes = new IRouter.Route[](1);
+
+        routes[0] = IRouter.Route({
+            from: address(WETH),
+            to: address(CbETH),
+            stable: false,
+            factory: AERODROME_FACTORY
+        });
+
+        vm.prank(OWNER);
+        adapter.setRoutes(WETH, CbETH, routes);
+
+        assertEq(adapter.getSwapRoutes(WETH, CbETH).length, 1);
+
+        vm.prank(OWNER);
+
+        vm.expectEmit();
+        emit RoutesRemoved(WETH, CbETH);
+
+        adapter.removeRoutes(WETH, CbETH);
+
+        assertEq(adapter.getSwapRoutes(WETH, CbETH).length, 0);
+    }
+
+    function test_removeRoutes_revertsWhen_calledByNonOwner() public {
+        IRouter.Route[] memory routes = new IRouter.Route[](1);
+
+        routes[0] = IRouter.Route({
+            from: address(WETH),
+            to: address(CbETH),
+            stable: false,
+            factory: AERODROME_FACTORY
+        });
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                OwnableUpgradeable.OwnableUnauthorizedAccount.selector,
+                NON_OWNER
+            )
+        );
+
+        vm.prank(NON_OWNER);
+        adapter.setRoutes(WETH, CbETH, routes);
+    }
+
+    function test_setIsPoolStable_setsValueForIsPoolStableForGivenTokens_andEmitsIsPoolStableSetEvent(
+    ) public {
+        assertEq(adapter.getIsPoolStable(WETH, CbETH), false);
+
+        vm.prank(OWNER);
+
+        vm.expectEmit();
+        emit IsPoolStableSet(WETH, CbETH, true);
+
+        adapter.setIsPoolStable(WETH, CbETH, true);
+
+        assertEq(adapter.getIsPoolStable(WETH, CbETH), true);
+    }
+
+    function test_setIsPoolStable_revertsWhen_CallerIsNotOwner() public {
+        vm.prank(NON_OWNER);
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                OwnableUpgradeable.OwnableUnauthorizedAccount.selector,
+                NON_OWNER
+            )
+        );
+
+        adapter.setIsPoolStable(WETH, CbETH, true);
+    }
+
+    function test_setRouter_setAddressForRouter_andEmitsRouterSetEvent()
+        public
+    {
+        assertEq(adapter.getRouter(), AERODROME_ROUTER);
+
+        vm.prank(OWNER);
+
+        vm.expectEmit();
+        emit RouterSet(OWNER);
+
+        adapter.setRouter(OWNER);
+    }
+
+    function test_setRouter_revertsWhen_CallerIsNotOwner() public {
+        vm.prank(NON_OWNER);
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                OwnableUpgradeable.OwnableUnauthorizedAccount.selector,
+                NON_OWNER
+            )
+        );
+
+        adapter.setRouter(OWNER);
+    }
+
+    function test_setPoolFactory_setAddressForPoolFactory_andEmitsPoolFactorySetEvent(
+    ) public {
+        assertEq(adapter.getPoolFactory(), AERODROME_FACTORY);
+
+        vm.prank(OWNER);
+
+        vm.expectEmit();
+        emit PoolFactorySet(OWNER);
+
+        adapter.setPoolFactory(OWNER);
+    }
+
+    function test_setPoolFactory_revertsWhen_CallerIsNotOwner() public {
+        vm.prank(NON_OWNER);
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                OwnableUpgradeable.OwnableUnauthorizedAccount.selector,
+                NON_OWNER
+            )
+        );
+
+        adapter.setPoolFactory(OWNER);
     }
 }
