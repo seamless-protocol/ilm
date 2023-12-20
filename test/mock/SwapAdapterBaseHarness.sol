6265ae68e88b1a3613a3a1cdeec7a808b3b3e2d3
NouDaimon <peric.iason@gmail.com>
2023-12-20 17:50:24 +0200

    build: `SwapAdapterBase` tests

---
+++ test/mock/SwapAdapterBaseHarness.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.18;
+
+import { IERC20 } from "@openzeppelin/contracts/interfaces/IERC20.sol";
+
+import { SwapAdapterBase } from "../../src/swap/adapter/SwapAdapterBase.sol";
+
+/// @title SwapAdapterBaseHarness
+/// @dev exposes SwapAdapterBase internal functions for testing
+contract SwapAdapterBaseHarness is SwapAdapterBase {
+    /// @dev exposes the _setSwapper internal function
+    function exposed_setSwapper(address swapper) external {
+        _setSwapper(swapper);
+    }
+
+    /// @dev exposes the _getSwapper internal function
+    function exposed_getSwapper() external view returns (address swapper) {
+        return _getSwapper();
+    }
+
+    /// @dev unimplemented in harness contract
+    function executeSwap(
+        IERC20 from,
+        IERC20 to,
+        uint256 fromAmount,
+        address payable beneficiary
+    ) external returns (uint256 toAmount) {
+        // unimplemented
+    }
+
+    /// @dev unimplemented in harness contract
+    function setSwapper(address swapper) external { }
+
+    /// @dev unimplemented in harness contract
+    function getSwapper() external view returns (address swapper) { }
+}
---
+++ test/unit/SwapAdapterBase.t.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.18;
+
+import { BaseForkTest } from "../BaseForkTest.t.sol";
+
+import { SwapAdapterBaseHarness } from "../mock/SwapAdapterBaseHarness.sol";
+
+/// @title SwapAdapterBase
+/// @notice Unit tests for the SwapAdapterBase contract
+contract SwapAdapterBase is BaseForkTest {
+    ///////////////////////////////////
+    //////// REPLICATED EVENTS ////////
+    ///////////////////////////////////
+
+    /// @notice emitted when the Swapper contract is set for a given adapter
+    /// @param swapper address of Swapper contract
+    event SwapperSet(address swapper);
+
+    address newSwapper = makeAddr('newSwapper');
+
+    SwapAdapterBaseHarness adapter;
+
+    function setUp() public {
+        adapter = new SwapAdapterBaseHarness();
+    }
+
+    function test_expoedSetSwapper_newSwapperAddressIsSet_and_SwapperSetEventIsEmitted() public {
+        assertEq(
+            adapter.exposed_getSwapper(),
+            address(0)
+        );
+
+        vm.expectEmit();
+        emit SwapperSet(newSwapper);
+
+        adapter.exposed_setSwapper(newSwapper);
+
+        assertEq(
+            adapter.exposed_getSwapper(),
+            newSwapper
+        );
+    }
+}
