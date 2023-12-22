68d3639f32ad891a90648b02be5dd5ed87c1f090
NouDaimon <81045441+NouDaimon@users.noreply.github.com>
2023-12-22 15:28:07 +0200

    `AerodromeAdapter` implementation and tests (#25)
    * refactor: update `ISwapAdaptor` function sig
    
    * refactor: move `Step` struct from `ISwapAdaptor` to `DataTypes, update `ISwapper` function sigs
    
    * feat: initial implementation of `Swapper` contract
    
    * feat: extend `Swapper` contract and `ISwapper` interface with events and additional `removeRoute` function
    
    * build: `Swapper` tests and mock contracts needed for testing
    
    * feat: `onlyStrategy` modifier
    
    * stlye: forge fmt
    
    * feat: `strategies` setters and getters
    
    * feat: add `strategy` settesr and getters to `ISwapper` interface
    
    * build: update old `Swapper` tests
    
    * build: add `strategy` setters and getters tests
    
    * feat: initial aerodrome interfaces
    
    * feat: initial AerofromeAdapter implementation contract and storage
    
    * feat: extend AerodromeAdapter with setters
    
    * build: initial `AerodromAdapter` tests
    
    * feat: `AerodromeAdapter` auxiliary functions and tests
    
    * style: add natspec for `AerodromeAdapter` tests
    
    * stlye: forge fmt
    
    * feat: `getSwapper` function in `ISwapAdapter`
    
    * feat: extend `ISwapAdapter` with custom error and setter for `swapper`
    
    * feat: `SwapAdapterBase` implementation and refactoring of `AerodromeAdapter` to use it
    
    * stlye: remove unused import
    
    * feat: IAerodromeAdpater interface
    
    * style: forge fmt
    
    * feat: refactor `AerodromAdapter` to properly use `SwapAdapterBase` contract and `IAerodromeAdapter` interface
    
    * style: forge fmt
    
    * build: `SwapAdapterBase` tests
    
    * style: forge fmt
    
    * build: add new test cases for `AerodromeAdapter`

---
+++ src/storage/AerodromeAdapterStorage.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.18;
+
+import { IERC20 } from "@openzeppelin/contracts/interfaces/IERC20.sol";
+
+import { IRouter } from "../vendor/aerodrome/IRouter.sol";
+
+library AerodromeAdapterStorage {
+    /// @dev struct containing all state for the Swapper contract
+    /// @custom:storage-location erc7201:seamless.contracts.storage.Swapper
+    struct Layout {
+        mapping(IERC20 from => mapping(IERC20 to => IRouter.Route[] routes))
+            swapRoutes;
+        mapping(IERC20 from => mapping(IERC20 to => bool isStable)) isPoolStable;
+        mapping(address pair => address factory) pairFactory;
+        address router;
+        address poolFactory;
+    }
+
+    // keccak256(abi.encode(uint256(keccak256("seamless.contracts.storage.AerodromeAdapter")) - 1)) & ~bytes32(uint256(0xff));
+    bytes32 internal constant STORAGE_SLOT =
+        0xe20fadfe51c05810cf8049153a6d3327f8bd31f8cadde6b0efd76ce5573f2600;
+
+    function layout() internal pure returns (Layout storage l) {
+        bytes32 slot = STORAGE_SLOT;
+        assembly {
+            l.slot := slot
+        }
+    }
+}
---
+++ src/swap/adapter/SwapAdapterBase.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.18;
+
+import {
+    Ownable2StepUpgradeable,
+    OwnableUpgradeable
+} from "@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
+import { IERC20 } from "@openzeppelin/contracts/interfaces/IERC20.sol";
+
+import { ISwapAdapter } from "../../interfaces/ISwapAdapter.sol";
+import { SwapAdapterBaseStorage as Storage } from
+    "../../storage/SwapAdapterBaseStorage.sol";
+
+/// @title SwapAdapterBase
+/// @notice Base adapter contract for all swap adapters
+/// @dev should be inherited and overridden by all SwapAdapter implementations
+abstract contract SwapAdapterBase is Ownable2StepUpgradeable, ISwapAdapter {
+    modifier onlySwapper() {
+        if (Storage.layout().swapper != msg.sender) {
+            revert NotSwapper();
+        }
+        _;
+    }
+
+    /// @notice swaps a given amount of a token to another token, sending the final amount to the beneficiary
+    /// @param from address of token to swap from
+    /// @param to address of token to swap to
+    /// @param fromAmount amount of from token to swap
+    /// @param beneficiary receiver of final to token amount
+    /// @return toAmount amount of to token returned from swapping
+    function _executeSwap(
+        IERC20 from,
+        IERC20 to,
+        uint256 fromAmount,
+        address payable beneficiary
+    ) internal virtual returns (uint256 toAmount) {
+        // override with adapter specific swap logic
+    }
+
+    /// @notice returns the address of Swapper contract
+    /// @return swapper address of Swapper contract
+    function _getSwapper() internal view virtual returns (address swapper) {
+        return Storage.layout().swapper;
+    }
+
+    /// @notice sets the address of the Swapper contract
+    /// @param swapper address of Swapper contract
+    function _setSwapper(address swapper) internal virtual {
+        Storage.layout().swapper = swapper;
+
+        emit SwapperSet(swapper);
+    }
+}
