4bbbbd6bf08936e28e01622b8e578b04ebd6a550
NouDaimon <81045441+NouDaimon@users.noreply.github.com>
2023-12-22 15:31:05 +0200

    Implementation for `WrappedToken` adapter (#26)
    * refactor: update `ISwapAdaptor` function sig
    
    * refactor: move `Step` struct from `ISwapAdaptor` to `DataTypes, update `ISwapper` function sigs
    
    * feat: initial implementation of `Swapper` contract
    
    * feat: extend `Swapper` contract and `ISwapper` interface with events and additional `removeRoute` function
    
    * build: `Swapper` tests and mock contracts needed for testing
    
    * feat: `onlyStrategy` modifier
    
    * stlye: forge fmt
    
    * feat: `strategies` setters and getters
    
    * feat: add `strategy` settesr and getters to `ISwapper` interface
    
    * build: update old `Swapper` tests
    
    * build: add `strategy` setters and getters tests
    
    * feat: initial aerodrome interfaces
    
    * feat: initial AerofromeAdapter implementation contract and storage
    
    * feat: extend AerodromeAdapter with setters
    
    * build: initial `AerodromAdapter` tests
    
    * feat: `AerodromeAdapter` auxiliary functions and tests
    
    * style: add natspec for `AerodromeAdapter` tests
    
    * stlye: forge fmt
    
    * feat: `getSwapper` function in `ISwapAdapter`
    
    * feat: extend `ISwapAdapter` with custom error and setter for `swapper`
    
    * feat: `SwapAdapterBase` implementation and refactoring of `AerodromeAdapter` to use it
    
    * stlye: remove unused import
    
    * feat: IAerodromeAdpater interface
    
    * style: forge fmt
    
    * feat: refactor `AerodromAdapter` to properly use `SwapAdapterBase` contract and `IAerodromeAdapter` interface
    
    * style: forge fmt
    
    * build: `SwapAdapterBase` tests
    
    * style: forge fmt
    
    * build: add new test cases for `AerodromeAdapter`
    
    * feat: initial implementation of `WrappedTokenAdapter`
    
    * style: forge fmt
    
    * build: `WrappedTokenAdapter` tests
    
    * feat: add `IWrappedTokenAdpater` interface and setters/getters for `swapper` in `WrappedTokenAdapter`
    
    * build: add new tests for `WrappedTokenAdapter`
    
    * style: forge fmt

---
+++ src/interfaces/IWrappedTokenAdapter.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.18;
+
+import { IERC20 } from "@openzeppelin/contracts/interfaces/IERC20.sol";
+
+import { IWrappedERC20PermissionedDeposit } from
+    "../interfaces/IWrappedERC20PermissionedDeposit.sol";
+
+/// @title IWrappedTokenAdapter
+/// @notice interface for WrappedTokenAdapter functionality
+interface IWrappedTokenAdapter {
+    /// @notice initializing function of adapter
+    /// @param owner address of adapter owner
+    function WrappedTokenAdapter__Init(address owner) external;
+
+    /// @notice sets the wrapper contract for a given token pair
+    /// @param from token to wrap/unwrap
+    /// @param to token received after wrapping/unwrapping
+    /// @param wrapper WrappedERC20PermissionedDeposit contract pertaining to from/to tokens
+    function setWrapper(
+        IERC20 from,
+        IERC20 to,
+        IWrappedERC20PermissionedDeposit wrapper
+    ) external;
+
+    /// @notice removes a previously set wrapper for a given from/to token pair
+    /// @param from token to wrap/unwrap
+    /// @param to token received after wrapping/unwrapping
+    function removeWrapper(IERC20 from, IERC20 to) external;
+
+    /// @notice returns wrapper contract for a given from/to token pair
+    /// @param from token to wrap/unwrap
+    /// @param to token received after wrapping/unwrapping
+    /// @return wrapper WrappedERC20PermissionedDeposit contract pertaining to from/to tokens
+    function getWrapper(IERC20 from, IERC20 to)
+        external
+        view
+        returns (IWrappedERC20PermissionedDeposit wrapper);
+}
