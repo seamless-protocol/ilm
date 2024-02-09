d768466d46ab557637e2f75936221e251f9fbfbc
NouDaimon <peric.iason@gmail.com>
2024-02-09 22:13:46 +0200

    build: rework `rebalance` script tests

--- keeper/test/rebalance.t.js
+++ keeper/test/rebalance.t.js
@@ -1,68 +1,63 @@
-const { expect } = require("chai");
-const { main } = require("../actions/rebalance");
-const { ethers, Contract } = require('ethers');
-const LoopStrategyJSON = require('../../out/LoopStrategy.sol/LoopStrategy.json');
+const { expect } = require('chai');
+const sinon = require('sinon');
+const { performRebalance } = require('../actions/rebalance'); 
 
-describe("rebalance action", function () {
-    let strategy, signer, snap;
-    // CbETH strategy on fork
-    const strategyAddress = '0x08dd8c0b5E660800970410f6Ab3e61727599501F';
-    // fork with FE testing (must be same as one in `rebalance.js` script)
-    const BASE_ILM_FORK_RPC_URL = 'https://rpc.tenderly.co/fork/1aec2019-21bb-4c84-a7c8-148fa6527483';
-    const provider = new ethers.providers.JsonRpcProvider(BASE_ILM_FORK_RPC_URL);
+describe('performRebalance', () => {
+    let strategyStub;
 
-    before(async function () {
-        this.timeout('10000');
-        signer = provider.getSigner('0xf6ded1795513c7744a3198a45b97dc55e4e12729');
-
-        strategy = new Contract(strategyAddress, LoopStrategyJSON.abi, signer);
+    beforeEach(() => {
+        strategyStub = {
+            rebalanceNeeded: sinon.stub(),
+            rebalance: sinon.stub()
+        };
+    });
+    
+    afterEach(() => {
+        sinon.restore();
     });
 
-    beforeEach(async () => {
-        snap = await provider.send("evm_snapshot", []);
+    it('should call rebalance if rebalanceNeeded returns true', async () => {
+      strategyStub.rebalanceNeeded.resolves(true);
+      strategyStub.rebalance.resolves({hash: 'txHash'});
+
+      const result = await performRebalance(strategyStub);
+
+      // assert that rebalanceNeeded was called
+      sinon.assert.calledOnce(strategyStub.rebalanceNeeded);
+
+      // assert that rebalance was called
+      sinon.assert.calledOnce(strategyStub.rebalance);
+
+      // Assert that correct transaction hash is returned
+      expect(result).to.deep.equal({ tx: 'txHash' });
     });
 
-    afterEach(async () => {
-        await provider.send("evm_revert", [snap]);
+    it('should not call rebalance if rebalanceNeeded returns false', async () => {
+    strategyStub.rebalanceNeeded.resolves(false);
+    
+    // call the performRebalance with stubs
+    const result = await performRebalance(strategyStub);
+
+    // assert that rebalanceNeeded was called
+    sinon.assert.calledOnce(strategyStub.rebalanceNeeded);
+
+    // assert that rebalance was not called
+    sinon.assert.notCalled(strategyStub.rebalance);
+
+    // assert that result is undefined
+    expect(result).to.be.undefined;
     });
 
-    // Emulates autotask run
-    const run = () => main(signer, strategyAddress);
-
-    it("does not rebalance if rebalanceNeeded is false", async function () {
-        this.timeout('10000');
-
-        expect(await strategy.rebalanceNeeded()).to.be.false;
-        const oldCR = await strategy.currentCollateralRatio();
-
-        await run();
-
-        expect(oldCR).to.deep.eq(await strategy.currentCollateralRatio());
-    });
-
-    it("rebalances if rebalanceNeeded is true", async function () {
-        this.timeout('10000');
-        let targets = await strategy.getCollateralRatioTargets();
-
-        const oldCR = await strategy.currentCollateralRatio();
-        const newTargets = [
-            ethers.BigNumber.from('170000000'),
-            ethers.BigNumber.from('167000000'),
-            ethers.BigNumber.from('173000000'),
-            ethers.BigNumber.from('170000000'),
-            ethers.BigNumber.from('170000000'),
-        ];
+    it('should handle errors', async () => {
+        strategyStub.rebalanceNeeded.rejects(new Error('error thrown'));
+        const consoleErrorStub = sinon.stub(console, 'error');
         
-        expect(await strategy.rebalanceNeeded()).to.be.false;
-
-        await strategy.setCollateralRatioTargets(newTargets);
-        targets = await strategy.getCollateralRatioTargets();
-
-        expect(await strategy.rebalanceNeeded()).to.be.true;
-
-        await run();
+        await performRebalance(strategyStub);
         
-        expect(ethers.BigNumber.from(oldCR)).to.not.eq(ethers.BigNumber.from(await strategy.currentCollateralRatio()));
-        expect(await strategy.rebalanceNeeded()).to.be.false;
+        sinon.assert.calledOnce(consoleErrorStub);
+        expect(consoleErrorStub.firstCall.args[0]).to.include('An error occurred on rebalance call:');
+        
+        console.error.restore();
     });
 });
+
