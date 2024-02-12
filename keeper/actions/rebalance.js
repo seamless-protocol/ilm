95c3955464f9ccc3883f5e5faf5b12cff73bd585
NouDaimon <peric.iason@gmail.com>
2024-02-12 17:17:03 +0200

    fix: get signer correctly

--- keeper/actions/rebalance.js
+++ keeper/actions/rebalance.js
@@ -2,16 +2,16 @@ const { ethers } = require("ethers");
 const { Defender } = require('@openzeppelin/defender-sdk');
 
 const strategyABI = ["function rebalanceNeeded() external view returns (bool)", "function rebalance() external returns (uint256)"];
-// on tenderly fork
+// REPLACE WITH STRATEGY ADDRESS OF INTEREST
 const strategyAddress = '0x08dd8c0b5E660800970410f6Ab3e61727599501F';
 
 // execute rebalance operation if its necessary
 async function performRebalance(strategy) {
   try {
     if (await strategy.rebalanceNeeded()) {
-        const tx = await strategy.rebalance();
-        console.log(`Called rebalance in ${tx.hash}`);
-        return { tx: tx.hash };
+      const tx = await strategy.rebalance();
+      console.log(`Called rebalance in ${tx.hash}`);
+      return { tx: tx.hash };
     } else {
       console.log('Rebalance not needed.');
     }
@@ -21,11 +21,14 @@ async function performRebalance(strategy) {
 }
 
 // Entrypoint for the action
-exports.handler = async function(credentials) {
+exports.handler = async function (credentials) {
   const client = new Defender(credentials);
 
-  const strategy = new ethers.Contract(strategyAddress, strategyABI, client.relaySigner,);
- 
+  const provider = client.relaySigner.getProvider();
+  const signer = client.relaySigner.getSigner(provider, { speed: 'fast' });
+
+  const strategy = new ethers.Contract(strategyAddress, strategyABI, signer);
+
   await performRebalance(strategy);
 }
 
