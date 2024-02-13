70188e37301358051bddde74894375d2e5c7a00a
NouDaimon <81045441+NouDaimon@users.noreply.github.com>
2024-02-13 22:29:56 +0200

    Adds OZ Defender action for rebalancing (#101)
    * init
    
    * style: forge fmt
    
    * feat: initial keeper action
    
    * refactor: extract `performRebalance` out of handler
    
    * build: packages for testing
    
    * feat: refactor action
    
    * build: add action tests
    
    * feat: check currentCR is not max when calling `rebalanceNeeded()`
    
    * feat: check `state.collateralUSD !=0 ` in `rebalanceNeeded`
    
    * feat: wrap all SC calls in `rebalance.js` in `try-catch`
    
    * build: add test command for keepers
    
    * refactor: rebalance script
    
    * build: add sinon
    
    * build: rework `rebalance` script tests
    
    * docs: short readme file
    
    * fix: get signer correctly
    
    * build: remove unused packages

--- test/integration/IntegrationBaseTest.sol
+++ test/integration/IntegrationBaseTest.sol
@@ -8,21 +8,20 @@ import { IntegrationBase } from "./IntegrationBase.sol";
 
 /// @notice Test confirming deployment is done correctly and we can deposit and redeem funds
 contract IntegrationBaseTest is IntegrationBase {
-
     /// @dev test confirming deployment is done correctly and we can deposit and redeem funds
     function test_integrationBaseTest() public {
-      address user = makeAddr("user");
+        address user = makeAddr("user");
 
-      uint256 amount = 1 ether;
+        uint256 amount = 1 ether;
 
-      vm.startPrank(user);
-      deal(address(CbETH), user, amount);
-      CbETH.approve(address(strategy), amount);
+        vm.startPrank(user);
+        deal(address(CbETH), user, amount);
+        CbETH.approve(address(strategy), amount);
 
-      uint256 shares = strategy.deposit(amount, user);
+        uint256 shares = strategy.deposit(amount, user);
 
-      strategy.redeem(shares / 2, user, user);
+        strategy.redeem(shares / 2, user, user);
 
-      vm.stopPrank();
+        vm.stopPrank();
     }
 }
