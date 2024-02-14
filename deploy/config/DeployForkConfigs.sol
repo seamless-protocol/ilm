6f3d026aab0e50cf637bc35ba1a60718d4956110
duxxud <12116005+duxxud@users.noreply.github.com>
2024-02-15 00:35:15 +0100

    `DeployHelper` upgrade and Simulation upgrade (#102)
    * fix: set offset factor when deploying
    
    * feat: base contract for integration tests
    
    * feat: e2e simulation
    
    * wip: measure accuracy of preview functions
    
    * chore: add comments
    
    * fix: deploy scripts constructors
    
    * fix: use `assets.collateral` in `convertCollateralToUnderlying`
    
    * feat: handle surplus equity in `rebalanceBeforeWithdraw`
    
    * Enforces a reversion in case the `minForRebalance` limit is breached (#88)
    
    * feat: add check for over exposure
    
    * build: update failing tests
    
    * build: add test case for `RatioOutsideRange`
    
    * build: remove duplicate test file
    
    * Reversion due to high slippage in `redeem` and `deposit` (#85)
    
    * feat: extend `SwapperStorage` to contain oracles and slippages for tokens
    
    * feat: setters/getters for new `Swapper` storage variables
    
    * feat: add slippage limit check
    
    * style: reorder functions
    
    * feat: alter how max slippage works
    
    * style: forge fmt
    
    * build: setter tests for new swapper functions
    
    * fix: minor issue in accounting for `fromAmount` in swap
    
    * build: add slippage to `SwapAdapterMock` and test slippage check in `Swapper`
    
    * feat: additional parameters in initializers
    
    * build: update tests
    
    * fix: maxDeviationUSD calclulation
    
    * style: forge fmt
    
    * build: test slippageTooHigh setup WIP
    
    * style: forge fmt
    
    * build: test `rebalanceUp` reversion when slippage is too high
    
    * fix: remove commented code
    
    * WIP: test helper
    
    * build: testFail case for `rebalanceDownToDebt` when slippage is too high
    
    * style: forge fmt
    
    * build: setup context for testing `MaxSlippageExceeded` reversion in tests
    
    * build: tests for slippage reversion in `deposit` and `redeem`
    
    * fix: typo, deposit reversion test case
    
    * Update fork block number and adjust test cases (#84)
    
    * build: update fork block number constant
    
    * build: update supply/borrow caps in lending pool
    
    * fix: WIP debugging LoanLogic tests
    
    * fix: `_changeSupplyAndBorrowCap` typo
    
    * style: forge fmt
    
    * fix: add `CbETH` supply and borrow caps in `LoopStrategy` constructor
    
    * fix: order of operations in convertUSDToAsset conversion
    
    * fix: previewRedeem tests precision
    
    * build: increase gap of slippage in slippage test
    
    * natspec: extend comment
    
    * fix: increase slippage in mockadapter
    
    * style: forge fmt
    
    ---------
    
    Co-authored-by: DuXXuD <duxxud@gmail.com>
    
    * build: add capacity for `SwapperMock` to have real/estimated offsets for testing
    
    * build: add tests for surplus equity due to swap
    
    * fix: remove console logs
    
    * fix: deploy config change
    
    * feat: add integration base test
    
    * fix: move out preview accuracy testing
    
    * fix: handle maxSlippageUSD = 0
    
    * chore: renaming
    
    * chore: move scenarios to unit folder
    
    * chore: remove unused imports
    
    * chore: gitignore simulation output
    
    * fix: moving scenario tests
    
    * fix: moving scenario tests
    
    * chore: imports fix
    
    * upd gitignore
    
    * gitignore: ignore simulation output
    
    * fix: import console
    
    * fix: gitignore test failing without dir
    
    * add gitignore simulation output back
    
    * deployments rework
    
    * chore: add DeployHeleper and configurations NatSpec
    
    * fix: _setupSwapperRoles function name
    
    * feat: MockSwapper handle swaps with the wrapped token
    
    * feat: Simulation update for better profitability testing
    
    * forge fmt
    
    * fix: remove duplicated code
    
    * fix: use wrappedToken decimals instead constant
    
    * Apply suggestions from code review
    
    Co-authored-by: NouDaimon <81045441+NouDaimon@users.noreply.github.com>
    
    * fix: add config comments
    
    ---------
    
    Co-authored-by: NouDaimon <peric.iason@gmail.com>
    Co-authored-by: NouDaimon <81045441+NouDaimon@users.noreply.github.com>

---
+++ deploy/config/DeployForkConfigs.sol
@@ -0,0 +1,112 @@
+// SPDX-License-Identifier: BUSL-1.1
+
+pragma solidity 0.8.21;
+
+import { LoopStrategyConfig, ERC20Config, ReserveConfig, CollateralRatioConfig, SwapperConfig } from "./LoopStrategyConfig.sol";
+import { CollateralRatio } from "../../src/types/DataTypes.sol";
+import { USDWadRayMath } from "../../src/libraries/math/USDWadRayMath.sol";
+
+/// @title DeployForkConfigs
+/// @notice contains configurations for the cbETH/ETH and wstETH/ETH LoopStrategies for the fork testing deployment
+contract DeployForkConfigs {
+
+  LoopStrategyConfig public cbETHconfig = LoopStrategyConfig({
+    // cbETH address
+    underlyingTokenAddress: 0x2Ae3F1Ec7F1F5012CFEab0185bfc7aa3cf0DEc22, 
+    // cbETH-USD Chainlink feed     
+    underlyingTokenOracle: 0xd7818272B9e248357d13057AAb0B417aF31E817d,        
+
+    strategyERC20Config: ERC20Config({
+      name: "CbETH/WETH Seamless ILM",
+      symbol: "ilmCbETH"
+    }),
+
+    wrappedTokenERC20Config: ERC20Config({
+      name: "ILM WrappedCbETH",
+      symbol: "wCbETH"
+    }),
+
+    wrappedTokenReserveConfig: ReserveConfig({
+      // cbETH interest rate strategy address, not important for this deploy
+      interestRateStrategyAddress: 0xcEd653F5C689eC80881b1A8b9Ab2b64DF2B963Bd,
+      aTokenName: "Seamless wrapped CbETH",
+      aTokenSymbol: "swCbETH",
+      variableDebtTokenName: "Seamless Variable Debt wrapped CbETH",
+      variableDebtTokenSymbol: "variableDebtSeamWCbETH",
+      stableDebtTokenName: "Seamless Stable Debt wrapped CbETH",
+      stableDebtTokenSymbol: "stableDebtSeamWCbETH",
+
+      ltv: 90_00,
+      liquidationTrashold: 92_00,
+      liquidationBonus: 100_00 + 5_00
+    }),
+
+    collateralRatioConfig: CollateralRatioConfig({
+      collateralRatioTargets: CollateralRatio({
+        target: USDWadRayMath.usdDiv(300, 200), // 3x
+        minForRebalance: USDWadRayMath.usdDiv(330, 230),  // 3.3x
+        maxForRebalance: USDWadRayMath.usdDiv(270, 170),  // 2.7x
+        maxForDepositRebalance: USDWadRayMath.usdDiv(299, 199), // 2.99x
+        minForWithdrawRebalance: USDWadRayMath.usdDiv(301, 201)  // 3.01x 
+      }),
+
+      ratioMargin: 10 ** 4, // 0.01% ratio margin
+      maxIterations: 10
+    }),
+
+    swapperConfig: SwapperConfig({
+      swapperOffsetFactor: 500000, // 0.5 %
+      swapperOffsetDeviation: 499000000 // 499% 
+    })
+  });
+
+  LoopStrategyConfig public wstETHconfig = LoopStrategyConfig({
+    // stETH address
+    underlyingTokenAddress: 0xc1CBa3fCea344f92D9239c08C0568f6F2F0ee452,
+    // stETH-USD Adapter oracle (used in the Seamless pool)
+    underlyingTokenOracle: 0xD815218fA8c9bd605c2b048f26cd374A752cAA76,
+
+    strategyERC20Config: ERC20Config({
+      name: "wstETH/WETH Seamless ILM",
+      symbol: "ilmwstETH"
+    }),
+
+    wrappedTokenERC20Config: ERC20Config({
+      name: "ILM WrappedwstETH",
+      symbol: "wwstETH"
+    }),
+
+    wrappedTokenReserveConfig: ReserveConfig({
+      // cbETH interest rate strategy address, not important for this deploy
+      interestRateStrategyAddress: 0xcEd653F5C689eC80881b1A8b9Ab2b64DF2B963Bd,
+      aTokenName: "Seamless wrapped wstETH",
+      aTokenSymbol: "swwstETH",
+      variableDebtTokenName: "Seamless Variable Debt wrapped wstETH",
+      variableDebtTokenSymbol: "variableDebtSeamWwstETH",
+      stableDebtTokenName: "Seamless Stable Debt wrapped wstETH",
+      stableDebtTokenSymbol: "stableDebtSeamWwstETH",
+
+      ltv: 90_00,
+      liquidationTrashold: 92_00,
+      liquidationBonus: 100_00 + 5_00
+    }),
+
+    collateralRatioConfig: CollateralRatioConfig({
+      collateralRatioTargets: CollateralRatio({
+        target: USDWadRayMath.usdDiv(300, 200), // 3x
+        minForRebalance: USDWadRayMath.usdDiv(301, 201),  // 3.01x
+        maxForRebalance: USDWadRayMath.usdDiv(299, 199),  // 2.99x
+        maxForDepositRebalance: USDWadRayMath.usdDiv(2999, 1999), // 2.999x
+        minForWithdrawRebalance: USDWadRayMath.usdDiv(3001, 2001)  // 3.001x 
+      }),
+
+      ratioMargin: 10 ** 4, // 0.01% ratio margin
+      maxIterations: 10
+    }),
+
+    swapperConfig: SwapperConfig({
+      swapperOffsetFactor: 500000, // 0.5 %
+      swapperOffsetDeviation: 499000000 // 499% 
+    })
+  });
+}
