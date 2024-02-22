064f53e1750a05a84df79e42264a0e1601d22eaa
DuXXuD <duxxud@gmail.com>
2024-02-22 22:31:32 +0100

    forge fmt

--- script/deploy/base-mainnet/DeployLoopStrategyWstETHoverETH.s.sol
+++ script/deploy/base-mainnet/DeployLoopStrategyWstETHoverETH.s.sol
@@ -6,8 +6,10 @@ import { Script } from "forge-std/Script.sol";
 import { IERC20 } from "@openzeppelin/contracts/interfaces/IERC20.sol";
 import { ISwapper, Swapper } from "../../../src/swap/Swapper.sol";
 import { LoopStrategy, ILoopStrategy } from "../../../src/LoopStrategy.sol";
-import { WrappedTokenAdapter } from "../../../src/swap/adapter/WrappedTokenAdapter.sol";
-import { AerodromeAdapter } from "../../../src/swap/adapter/AerodromeAdapter.sol";
+import { WrappedTokenAdapter } from
+    "../../../src/swap/adapter/WrappedTokenAdapter.sol";
+import { AerodromeAdapter } from
+    "../../../src/swap/adapter/AerodromeAdapter.sol";
 import { DeployHelper } from "../DeployHelper.s.sol";
 import { WrappedERC20PermissionedDeposit } from
     "../../../src/tokens/WrappedERC20PermissionedDeposit.sol";
@@ -23,9 +25,8 @@ import { USDWadRayMath } from "../../../src/libraries/math/USDWadRayMath.sol";
 import { BaseMainnetConstants } from "../config/BaseMainnetConstants.sol";
 
 contract LoopStrategyWstETHoverETHConfig is BaseMainnetConstants {
-
-    WrappedERC20PermissionedDeposit public wrappedToken = 
-        WrappedERC20PermissionedDeposit(0xc9ae3B5673341859D3aC55941D27C8Be4698C9e4);
+    WrappedERC20PermissionedDeposit public wrappedToken =
+    WrappedERC20PermissionedDeposit(0xc9ae3B5673341859D3aC55941D27C8Be4698C9e4);
 
     uint256 public assetsCap = 35 ether;
 
@@ -38,8 +39,10 @@ contract LoopStrategyWstETHoverETHConfig is BaseMainnetConstants {
             name: "Seamless ILM 3x Loop wstETH/ETH",
             symbol: "ilm-wstETH/ETH-3xloop"
         }),
-        wrappedTokenERC20Config: ERC20Config("",""), // empty, not used
-        wrappedTokenReserveConfig: ReserveConfig(address(0),"","","","","","",0,0,0), // empty, not used
+        wrappedTokenERC20Config: ERC20Config("", ""), // empty, not used
+        wrappedTokenReserveConfig: ReserveConfig(
+            address(0), "", "", "", "", "", "", 0, 0, 0
+            ), // empty, not used
         collateralRatioConfig: CollateralRatioConfig({
             collateralRatioTargets: CollateralRatio({
                 target: USDWadRayMath.usdDiv(150, 100), // 1.5
@@ -58,8 +61,11 @@ contract LoopStrategyWstETHoverETHConfig is BaseMainnetConstants {
     });
 }
 
-contract DeployLoopStrategyWstETHoverETH is Script, DeployHelper, LoopStrategyWstETHoverETHConfig {
-
+contract DeployLoopStrategyWstETHoverETH is
+    Script,
+    DeployHelper,
+    LoopStrategyWstETHoverETHConfig
+{
     function run() public {
         uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PK");
         address deployerAddress = vm.addr(deployerPrivateKey);
@@ -67,12 +73,14 @@ contract DeployLoopStrategyWstETHoverETH is Script, DeployHelper, LoopStrategyWs
         vm.startBroadcast(deployerPrivateKey);
 
         Swapper swapper = _deploySwapper(
-            deployerAddress, wstETHoverETHconfig.swapperConfig.swapperOffsetDeviation
+            deployerAddress,
+            wstETHoverETHconfig.swapperConfig.swapperOffsetDeviation
         );
 
-        (WrappedTokenAdapter wrappedTokenAdapter, AerodromeAdapter aerodromeAdapter) = _deploySwapAdapters(
-            swapper, wrappedToken, deployerAddress
-        );
+        (
+            WrappedTokenAdapter wrappedTokenAdapter,
+            AerodromeAdapter aerodromeAdapter
+        ) = _deploySwapAdapters(swapper, wrappedToken, deployerAddress);
 
         _setupSwapperRoutes(
             Swapper(address(swapper)),
@@ -91,12 +99,22 @@ contract DeployLoopStrategyWstETHoverETH is Script, DeployHelper, LoopStrategyWs
         _setupSwapperRoles(Swapper(address(swapper)), strategy);
 
         // set admin roles on swapper
-        swapper.grantRole(wrappedToken.DEFAULT_ADMIN_ROLE(), SEAMLESS_GOV_SHORT_TIMELOCK_ADDRESS);
-        swapper.grantRole(wrappedToken.DEFAULT_ADMIN_ROLE(), SEAMLESS_COMMUNITY_MULTISIG);
+        swapper.grantRole(
+            wrappedToken.DEFAULT_ADMIN_ROLE(),
+            SEAMLESS_GOV_SHORT_TIMELOCK_ADDRESS
+        );
+        swapper.grantRole(
+            wrappedToken.DEFAULT_ADMIN_ROLE(), SEAMLESS_COMMUNITY_MULTISIG
+        );
 
         // set admin roles on strategy
-        strategy.grantRole(wrappedToken.DEFAULT_ADMIN_ROLE(), SEAMLESS_GOV_SHORT_TIMELOCK_ADDRESS);
-        strategy.grantRole(wrappedToken.DEFAULT_ADMIN_ROLE(), SEAMLESS_COMMUNITY_MULTISIG);
+        strategy.grantRole(
+            wrappedToken.DEFAULT_ADMIN_ROLE(),
+            SEAMLESS_GOV_SHORT_TIMELOCK_ADDRESS
+        );
+        strategy.grantRole(
+            wrappedToken.DEFAULT_ADMIN_ROLE(), SEAMLESS_COMMUNITY_MULTISIG
+        );
 
         // transfer ownership on token adapters
         wrappedTokenAdapter.transferOwnership(SEAMLESS_COMMUNITY_MULTISIG);
@@ -104,8 +122,9 @@ contract DeployLoopStrategyWstETHoverETH is Script, DeployHelper, LoopStrategyWs
 
         // renounce deployer admin roles
         swapper.renounceRole(wrappedToken.DEFAULT_ADMIN_ROLE(), deployerAddress);
-        strategy.renounceRole(wrappedToken.DEFAULT_ADMIN_ROLE(), deployerAddress);
-
+        strategy.renounceRole(
+            wrappedToken.DEFAULT_ADMIN_ROLE(), deployerAddress
+        );
 
         vm.stopBroadcast();
     }
