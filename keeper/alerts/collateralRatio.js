59893ca71b38f1805e6418c3d707218c48a502e3
NouDaimon <peric.iason@gmail.com>
2024-02-23 16:32:44 +0200

    feat: initial `collateralRatio` alert

--- keeper/alerts/collateralRatio.js
+++ keeper/alerts/collateralRatio.js
@@ -12,9 +12,40 @@ async function isStrategyOverexposed(strategy) {
     try {
         const currentCR = await strategy.currentCollateralRatio();
         const minForRebalance = (await strategy.getCollateralRatioTargets())[1];
-
+        
+       return {
+            isOverExposed: currentCR < minForRebalance,
+            current: currentCR,
+            min: minForRebalance
+       };
         
     } catch (err) {
         console.error('An error has occured during collateral ratio checl: ', err);
+    }
+}
+
+exports.handler = async function (credentials, context) {
+    const client = new Defender(credentials);
+
+    // checkAlertChannelsExist(client); can add later
+    const { notificationClient } = context;
+
+    const provider = client.relaySigner.getProvider();
+    const signer = client.relaySigner.getSigner(provider, { speed: 'fast' });
+
+    const strategy = new ethers.Contract(strategyAddress, strategyABI, signer);
+
+    const { isOverExposed, currentCR, minForRebalance } = isStrategyOverexposed(strategy);
+
+    if (isOverExposed) {
+        try {
+            notificationClient.send({
+                channelAlias: 'seamless-alerts',
+                subject: 'STRATEGY IS OVEREXPOSED',
+                message: `Current collateral ratio is ${currentCR} and minForRebalance ratio is ${minForRebalance} `,
+            });
+        } catch (error) {
+            console.error('Failed to send notification', error);
+        }
     }
 }
