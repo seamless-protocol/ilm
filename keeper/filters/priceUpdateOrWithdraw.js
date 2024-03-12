bd245001965b7490d63eb4c3a4ba352f451d6af6
NouDaimon <peric.iason@gmail.com>
2024-03-12 11:57:52 +0200

    fix: custom filter

--- keeper/filters/priceUpdateOrWithdraw.js
+++ keeper/filters/priceUpdateOrWithdraw.js
@@ -2,22 +2,31 @@
 const withdrawSig = 'Withdraw(address,address,address,uint256,uint256)';
 const priceUpdateSig = 'AnswerUpdated(int256,uint256,uint256)';
 
-exports.handler = async function(payload) {
+exports.handler = async function (payload) {
     const conditionRequest = payload.request.body;
     const matches = [];
     const events = conditionRequest.events;
-    for(const evt of events) {
-      if (evt.eventSignature == priceUpdateSig || evt.eventSignature == withdrawSig) {
-        let notificationType = evt.eventSignature === priceUpdateSig ? 'priceUpdate' : 'withdarwal';
-        
-        matches.push({
-            hash: evt.hash,
-            metadata: {
-             "type": notificationType,
-            }
-         });
-      }
+    for (let evt of events) {
+        if(evt.matchReasons.signature == withdrawSig) {
+            matches.push({
+                hash: evt.hash,
+                metadata: {
+                    "notificationType": "withdrawal",
+                    "strategy": evt.matchedChecksumAddress
+                }
+             });
+        }
+
+        if(evt.matchReasons.signature == priceUpdateSig) {
+            matches.push({
+                hash: evt.hash,
+                metadata: {
+                    "notificationType": "priceUpdate",
+                    "oracle": evt.matchedChecksumAddress
+                }
+            })
+        }
     }
     return { matches }
-  }
+}
 
