34e41c27daac8a65caebfe62c16c0c0116ccf312
NouDaimon <peric.iason@gmail.com>
2024-04-03 09:04:15 +0300

    feat: make `borrowRate` check accept different values per strategy

--- keeper/filters/loopStrategy.js
+++ keeper/filters/loopStrategy.js
@@ -3,7 +3,6 @@ const { ethers } = require("ethers");
 const { KeyValueStoreClient } = require('defender-kvstore-client');
 const { sendOracleOutageAlert, sendExposureAlert, sendHealthFactorAlert, sendEPSAlert, sendSequencerOutageAlert, sendBorrowRateNotification } = require("./utils");
 
-
 const depositSig = 'Deposit(address,address,uint256,uint256)';
 const withdrawSig = 'Withdraw(address,address,address,uint256,uint256)';
 const priceUpdateSig = 'AnswerUpdated(int256,uint256,uint256)';
@@ -18,6 +17,16 @@ const poolSupplySig = 'Supply(address,address,address,uint256,uint16)';
 const oracleToStrategies = {
     "0xa669E5272E60f78299F4824495cE01a3923f4380": ["0x258730e23cF2f25887Cb962d32Bd10b878ea8a4e"],
     "0x71041dddad3595F9CEd3DcCFBe3D1F4b0a16Bb70": ["0x258730e23cF2f25887Cb962d32Bd10b878ea8a4e"],
+};
+
+// 0x258730e23cF2f25887Cb962d32Bd10b878ea8a4e: 3x wstETH/WETH
+const strategyInterestThreshold = {
+    "0x258730e23cF2f25887Cb962d32Bd10b878ea8a4e": ethers.BigNumber.from(ethers.utils.parseUnits('3.0', 27)), // 3% in RAY
+};
+
+// 0x4200000000000000000000000000000000000006: WETH
+const debtTokenToStrategies = {
+    "0x4200000000000000000000000000000000000006": ["0x258730e23cF2f25887Cb962d32Bd10b878ea8a4e"],
 };
 
 const strategyABI = [
@@ -41,8 +50,6 @@ const poolABI = [
 const RPC_URL = 'some_rpc_url';
 
 const healthFactorThreshold = ethers.BigNumber.from(ethers.utils.parseUnits('1.1', 8)); //value used for testing
-// TODO: make different for each strategies 
-const borrowRateThreshold = ethers.BigNumber.from(ethers.utils.parseUnits('3.0', 27)); // 3% in RAY
 
 exports.handler = async function (payload, context) {
     const conditionRequest = payload.request.body;
@@ -111,13 +118,20 @@ exports.handler = async function (payload, context) {
                 // to query
                 const reserveAddress = reason.args[0];
 
-                let pool = new ethers.Contract(ethers.utils.getAddress(reason.address), poolABI, provider);
+                if (reserveAddress in debtTokenToStrategies) {
+                    let pool = new ethers.Contract(ethers.utils.getAddress(reason.address), poolABI, provider);
 
-                let reserveData = await pool.getReserveData(reserveAddress);
-                let variableBorrowRate = reserveData[3];
+                    let reserveData = await pool.getReserveData(reserveAddress);
+                    let variableBorrowRate = reserveData[3];
+                    
+                    for(let strategy of debtTokenToStrategies[reserveAddress]) {
+                        // send notification if interest rate is above threshold
+                        await sendBorrowRateNotification(notificationClient, variableBorrowRate, strategyInterestThreshold[strategy]);
+                    }
+                    
+                }
 
-                // send notification if interest rate is above threshold
-                await sendBorrowRateNotification(notificationClient, variableBorrowRate, borrowRateThreshold);
+                
             }
         }
     }
