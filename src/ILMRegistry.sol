62cb8e05b18c903267bc39003c1f29e5040fa99f
DuXXuD <duxxud@gmail.com>
2024-04-19 17:34:18 +0200

    fmt

--- src/ILMRegistry.sol
+++ src/ILMRegistry.sol
@@ -3,39 +3,40 @@
 pragma solidity ^0.8.21;
 
 import { AccessControl } from "@openzeppelin/contracts/access/AccessControl.sol";
-import { EnumerableSet } from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import { EnumerableSet } from
+    "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 
 /// @title ILMRegistry
 /// @notice Registry for the deployed ILMs
 contract ILMRegistry is AccessControl {
-  using EnumerableSet for EnumerableSet.AddressSet;
+    using EnumerableSet for EnumerableSet.AddressSet;
 
-  /// @dev role which can change strategy parameters
-  bytes32 public constant MANAGER_ROLE = keccak256("MANAGER_ROLE");
+    /// @dev role which can change strategy parameters
+    bytes32 public constant MANAGER_ROLE = keccak256("MANAGER_ROLE");
 
-  EnumerableSet.AddressSet private ilmSet;
+    EnumerableSet.AddressSet private ilmSet;
 
-  constructor(address initialAdmin) {
-    _grantRole(DEFAULT_ADMIN_ROLE, initialAdmin);
-  }
+    constructor(address initialAdmin) {
+        _grantRole(DEFAULT_ADMIN_ROLE, initialAdmin);
+    }
 
-  function addILM(address ilmAddress) external onlyRole(MANAGER_ROLE) {
-    ilmSet.add(ilmAddress);
-  }
+    function addILM(address ilmAddress) external onlyRole(MANAGER_ROLE) {
+        ilmSet.add(ilmAddress);
+    }
 
-  function removeILM(address ilmAddress) external onlyRole(MANAGER_ROLE) {
-    ilmSet.remove(ilmAddress);
-  }
+    function removeILM(address ilmAddress) external onlyRole(MANAGER_ROLE) {
+        ilmSet.remove(ilmAddress);
+    }
 
-  function countILM() external view returns (uint256) {
-    return ilmSet.length();
-  }
+    function countILM() external view returns (uint256) {
+        return ilmSet.length();
+    }
 
-  function getAllILMs() external view returns (address[] memory) {
-    return ilmSet.values();
-  }
+    function getAllILMs() external view returns (address[] memory) {
+        return ilmSet.values();
+    }
 
-  function getILMat(uint256 index) external view returns (address) {
-    return ilmSet.at(index);
-  }
+    function getILMat(uint256 index) external view returns (address) {
+        return ilmSet.at(index);
+    }
 }
