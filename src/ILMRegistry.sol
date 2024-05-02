bf508b7cf73c5606ca1053dc0ee3085fca1682fa
NouDaimon <peric.iason@gmail.com>
2024-05-02 10:38:56 +0300

    feat: add `IILMRegistry` interface

--- src/ILMRegistry.sol
+++ src/ILMRegistry.sol
@@ -8,34 +8,45 @@ import { EnumerableSet } from
 
 /// @title ILMRegistry
 /// @notice Registry for the deployed ILMs
-contract ILMRegistry is AccessControl {
+contract ILMRegistry is AccessControl, IILMRegistry {
     using EnumerableSet for EnumerableSet.AddressSet;
 
     /// @dev role which can change strategy parameters
     bytes32 public constant MANAGER_ROLE = keccak256("MANAGER_ROLE");
 
+    /// @dev holds all ilm Proxy contract addresses
     EnumerableSet.AddressSet private ilmSet;
 
     constructor(address initialAdmin) {
         _grantRole(DEFAULT_ADMIN_ROLE, initialAdmin);
+        _grantRole(MANAGER_ROLE, initialAdmin);
     }
 
+    /// @inheritdoc IILMRegistry
     function addILM(address ilmAddress) external onlyRole(MANAGER_ROLE) {
         ilmSet.add(ilmAddress);
+
+        emit ILMAdded(ilmAddress);
     }
 
+    /// @inheritdoc IILMRegistry
     function removeILM(address ilmAddress) external onlyRole(MANAGER_ROLE) {
         ilmSet.remove(ilmAddress);
+
+        emit ILMRemoved(ilmAddress);
     }
 
+    /// @inheritdoc IILMRegistry
     function countILM() external view returns (uint256) {
         return ilmSet.length();
     }
 
+    /// @inheritdoc IILMRegistry
     function getAllILMs() external view returns (address[] memory) {
         return ilmSet.values();
     }
 
+    /// @inheritdoc IILMRegistry
     function getILMat(uint256 index) external view returns (address) {
         return ilmSet.at(index);
     }
---
+++ src/interfaces/IILMRegistry.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: BUSL-1.1
+
+pragma solidity ^0.8.21;
+
+
+/// @title IILMRegistry
+/// @notice Interface for ILMRegistry
+interface IILMRegistry {
+
+    /// @notice emitted when a new ILM proxy contract address is added to
+    /// the ilmSet
+    /// @param ilm address of ILM proxy contract
+    event ILMAdded(address ilm);
+
+    /// @notice emitted when an ILM proxy contract address is remove from
+    /// the ilmSet
+    /// @param ilm address of ILM proxy contract
+    event ILMRemoved(address ilm);
+
+    /// @notice adds the address of the ILM proxy contract to the ilmSet
+    function addILM(address ilmAddress) external;
+
+    /// @notice removes the address of the ILM proxy contract to the ilmSet
+    function removeILM(address ilmAddress) external;
+
+    /// @notice adds the address of the ILM proxy contract to the ilmSet
+    /// @return ilmCount number of registered ilms
+    function countILM() external view returns (uint256 ilmCount);
+
+    /// @notice returns all registered ILM proxy contract addresses
+    /// @return ilms registered ILM proxy contract addresses
+    function getAllILMs() external view returns (address[] memory ilms);
+
+    /// @notice returns the address of the ILM proxy contract at the specified index
+    /// of the ilmSet
+    /// @param index index of ILM proxy contract
+    /// @return ilm address of ILM proxy contract
+    function getILM(uint256 index) external view returns (address ilm);
+}
