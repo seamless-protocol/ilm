480da0120260c89598ae0d8f1f388f90183fe176
NouDaimon <peric.iason@gmail.com>
2024-05-08 16:18:30 +0300

    style: forge fmt

--- script/deploy/base-mainnet/DeployILMRegistry.s.sol
+++ script/deploy/base-mainnet/DeployILMRegistry.s.sol
@@ -11,7 +11,8 @@ import { BaseMainnetConstants } from "../config/BaseMainnetConstants.sol";
 contract DeployILMRegistry is Script, BaseMainnetConstants {
     address internal constant WSTETHETHLOOP =
         0x258730e23cF2f25887Cb962d32Bd10b878ea8a4e;
-    address internal constant ETHUSDCLOOP = 0x2FB1bEa0a63F77eFa77619B903B2830b52eE78f4;
+    address internal constant ETHUSDCLOOP =
+        0x2FB1bEa0a63F77eFa77619B903B2830b52eE78f4;
 
     function run() public {
         uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PK");
@@ -43,21 +44,18 @@ contract DeployILMRegistry is Script, BaseMainnetConstants {
         );
         assert(
             registry.hasRole(
-                registry.MANAGER_ROLE(),
-                SEAMLESS_GOV_SHORT_TIMELOCK_ADDRESS
+                registry.MANAGER_ROLE(), SEAMLESS_GOV_SHORT_TIMELOCK_ADDRESS
             )
         );
 
         assert(
             registry.hasRole(
-                registry.DEFAULT_ADMIN_ROLE(),
-                SEAMLESS_COMMUNITY_MULTISIG
+                registry.DEFAULT_ADMIN_ROLE(), SEAMLESS_COMMUNITY_MULTISIG
             )
         );
-         assert(
+        assert(
             registry.hasRole(
-                registry.MANAGER_ROLE(),
-                SEAMLESS_COMMUNITY_MULTISIG
+                registry.MANAGER_ROLE(), SEAMLESS_COMMUNITY_MULTISIG
             )
         );
 
@@ -65,18 +63,8 @@ contract DeployILMRegistry is Script, BaseMainnetConstants {
         registry.revokeRole(registry.DEFAULT_ADMIN_ROLE(), deployer);
         vm.stopBroadcast();
 
-        assert(
-            !registry.hasRole(
-                registry.DEFAULT_ADMIN_ROLE(),
-               deployer
-            )
-        );
-         assert(
-            !registry.hasRole(
-                registry.MANAGER_ROLE(),
-                deployer
-            )
-        );
+        assert(!registry.hasRole(registry.DEFAULT_ADMIN_ROLE(), deployer));
+        assert(!registry.hasRole(registry.MANAGER_ROLE(), deployer));
 
         console.log("ILM Registry deployed at: ", address(registry));
     }
